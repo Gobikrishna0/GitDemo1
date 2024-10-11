f00d1ab35006dbf6862d2bbb9a40924296c4077d
Gobi <gobikrishna123456@gmail.com>
2024-10-11 10:30:17 +0530

    Adding my project

---
+++ src/test/java/com/telusko/GitDemo1/GitDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.GitDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
